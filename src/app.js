cad34ab8ec43d8a835342900f12a7d9918e400e8
Yeyul Choi <yeyulchoi2@gmail.com>
2025-11-02 16:55:52 -0500

    remove /api from the router

--- src/app.js
+++ src/app.js
@@ -22,10 +22,10 @@ app.use(express.json());
 
 
 
-app.use("/api", authRouter);
-app.use("/api", profileRouter);
-app.use("/api", requestsRouter);
-app.use("/api", userRouter);
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestsRouter);
+app.use("/", userRouter);
 
 
 app.get("/users", async (req, res) => {
